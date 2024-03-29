b5b103e8cf3439887c49885f004c45db45425be8
pknowles <pknowles@users.noreply.github.com>
2024-03-28 18:24:19 -0700

    movable linear_memory_resource

--- include/decodeless/allocator.hpp
+++ include/decodeless/allocator.hpp
@@ -15,6 +15,17 @@
 
 namespace decodeless {
 
+template <class Resource>
+concept ResourceType = requires(Resource resource) {
+    {
+        resource.allocate(std::declval<std::size_t>(), std::declval<std::size_t>())
+    } -> std::same_as<void*>;
+    {
+        resource.deallocate(std::declval<void*>(),
+                             std::declval<std::size_t>())
+    } -> std::same_as<void>;
+};
+
 template <class Allocator>
 concept AllocatorType = requires(Allocator allocator, typename Allocator::value_type) {
     {
@@ -59,7 +70,7 @@ public:
     using parent_allocator = ParentAllocator;
     linear_memory_resource() = delete;
     linear_memory_resource(const linear_memory_resource& other) = delete;
-    linear_memory_resource(linear_memory_resource&& other) noexcept = delete;
+    linear_memory_resource(linear_memory_resource&& other) noexcept = default;
     linear_memory_resource(size_t                 initialSize = INITIAL_SIZE,
                            const ParentAllocator& parentAllocator = ParentAllocator())
         : m_parentAllocator(parentAllocator)
@@ -68,7 +79,7 @@ public:
         , m_end(reinterpret_cast<uintptr_t>(m_begin) + initialSize) {}
     ~linear_memory_resource() { m_parentAllocator.deallocate(m_begin, bytesAllocated()); }
     linear_memory_resource& operator=(const linear_memory_resource& other) = delete;
-    linear_memory_resource& operator=(linear_memory_resource&& other) noexcept = delete;
+    linear_memory_resource& operator=(linear_memory_resource&& other) noexcept = default;
 
     [[nodiscard]] constexpr void* allocate(std::size_t bytes, std::size_t align) {
         // Align
@@ -127,29 +138,29 @@ private:
 
 // Workaround copyable STL allocators by passing around a pointer/reference to
 // the object with state.
-template <TriviallyDestructible T, class MemoryResource>
+template <TriviallyDestructible T, ResourceType MemoryResource>
 class aligned_allocator_ref {
 public:
     using memory_resource = MemoryResource;
     using value_type = T;
 
     aligned_allocator_ref(memory_resource& resource)
-        : m_resource(resource) {}
+        : m_resource(&resource) {}
 
     // Needed by msvc
     template <class U>
     aligned_allocator_ref(const aligned_allocator_ref<U, MemoryResource>& other)
-        : m_resource(other.resource()) {}
+        : m_resource(&other.resource()) {}
 
     [[nodiscard]] constexpr T* allocate(std::size_t n) {
-        return static_cast<T*>(m_resource.allocate(n * sizeof(T), alignof(T)));
+        return static_cast<T*>(m_resource->allocate(n * sizeof(T), alignof(T)));
     }
 
     constexpr void deallocate(T* p, std::size_t n) {
-        return m_resource.deallocate(static_cast<void*>(p), n);
+        return m_resource->deallocate(static_cast<void*>(p), n);
     }
 
-    memory_resource& resource() const { return m_resource; }
+    memory_resource& resource() const { return *m_resource; }
 
     // Needed by msvc
     template <class U>
@@ -158,22 +169,22 @@ public:
     };
 
 protected:
-    memory_resource& m_resource;
+    memory_resource* m_resource;
 };
 
 // STL compatible allocator with an implicit constructor from
 // linear_memory_resource. Emphasizes why std::pmr is a thing - ParentAllocator
 // shouldn't affect the type.
-template <TriviallyDestructible T, class MemoryResource = linear_memory_resource<>>
+template <TriviallyDestructible T, ResourceType MemoryResource = linear_memory_resource<>>
 using linear_allocator = aligned_allocator_ref<T, MemoryResource>;
 
-template <class T, class MemoryResource, class... Args>
+template <class T, ResourceType MemoryResource, class... Args>
 T* create(MemoryResource& memoryResource, Args&&... args) {
     return std::construct_at<T>(linear_allocator<T, MemoryResource>(memoryResource).allocate(1),
                                 std::forward<Args>(args)...);
 };
 
-template <class T, class MemoryResource>
+template <class T, ResourceType MemoryResource>
 std::span<T> createArray(MemoryResource& memoryResource, size_t size) {
     auto result =
         std::span(linear_allocator<T, MemoryResource>(memoryResource).allocate(size), size);
@@ -183,7 +194,7 @@ std::span<T> createArray(MemoryResource& memoryResource, size_t size) {
 };
 
 #ifdef __cpp_lib_ranges
-template <std::ranges::input_range Range, class MemoryResource>
+template <std::ranges::input_range Range, ResourceType MemoryResource>
 std::span<std::ranges::range_value_t<Range>> createArray(MemoryResource& memoryResource,
                                                          const Range&    range) {
     using T = std::ranges::range_value_t<Range>;
